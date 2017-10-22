c3d0a03daaaaa0541b126190dfea353d3bbc9dd0
monk <monk@cubee.com>
2017-10-22 23:49:56 +0800

    upate

--- src/game/halloween.js
+++ src/game/halloween.js
@@ -1,39 +1,37 @@
 /**
  * 万圣节活动收获南瓜游戏
  */
-
+let bgImgurl0 = require('../assets/img/game/halloween-bg1.png')
 export default function Halloween (El, config) {
   console.log('开始绘制图形')
   // 设置画布尺寸
-  console.log('El', El)
   let size = setCanvasSize(El)
   console.log('%c size', 'color: red', size)
   // 获取画布上下文
   let ctx = El.getContext('2d')
-  console.log(ctx)
   // 检测浏览器是否支持canvas
   if (!ctx) {
     alert('浏览器版本太低！')
     return null
   }
-  let offset = 0
+  // 加载图片
+  let bgImg1 = new Image()
+  bgImg1.src = bgImgurl0
   // 绘制图形
   function draw () {
     ctx.clearRect(0, 0, size.width, size.height)
-    ctx.setLineDash([4, 2])
-    ctx.lineDashOffset = -offset
-    ctx.strokeRect(10, 10, 100, 100)
+    ctx.drawImage(bgImg1, 0, 0, size.width, size.height)
   }
 
-  function march () {
-    offset++
-    if (offset > 16) {
-      offset = 0
-    }
+  // 更新数据
+  function animation () {
     draw()
-    setTimeout(march, 20)
+    window.requestAnimationFrame(animation)
   }
-  march()
+
+  bgImg1.onload = function () {
+    animation()
+  }
 }
 
 // 设置画布尺寸
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -26,5 +26,6 @@ export default {
 .home {
   width: 100%;
   height: 100%;
+  overflow: hidden;
 }
 </style>
